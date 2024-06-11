52e88b72056f8bf88d3479b44e8e76a22ab9ad74
kafkaarko <kafkaarko@gmail.com>
2024-06-11 21:27:32 +0700

    laravel pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+<!-- Menghubungkan dengan view template master -->
+@extends('master')
+ 
+@section('konten')
+        <div class="" style="background: #3572EF">
+            <div class="d-flex justify-content-center">
+            <div class="card text-white cd" style="width: 18rem; height:25rem; margin-top:4rem; background:#3ABEF9">
+                <div class="d-flex justify-content-center">
+
+                    <img src="assets/img/kafka.JPG" class="card-img-top" width="400" height="200" alt="...">
+                </div>
+                <div class="card-body text-center ">
+                <h5 class="card-title ">kafka arko ardissya</h5>
+                <p class="card-text">Seorang Siswa Wikrama</p>
+                </div>
+            </div>
+            <div class=" w-50">
+                <div class=" w-20 p-3">
+                  <div class="mt-4 p-5 text-white rounded" style="background: #3ABEF9">
+                    <h2>Proggrammer Pemula</h2> 
+                    <p>Seorang Siswa yang baru terjun ke dunia IT dan mau memperdalam ilmu coding</p> 
+                  </div>
+                </div>
+                <div class=" w-20 p-3">
+                  <div class="mt-4 p-5  text-white rounded" style="background: #3ABEF9"> 
+                    <h3>Keinginan</h3> 
+                    <p>Kafka ingin menjadi seorang Back-end development,karena itu sebuah perkerjaan yang di inginkan oleh kafka</p> 
+                  </div>
+                </div>
+            </div>
+        </div>
+        </div>
+      <!-- Short Description -->
+      <hr>
+      <!-- Skill Table -->
+      <div class="d-flex justify-content-center " style="background: #3572EF">
+
+     
+      <div class="">
+          <h3 class=" text-center text-white">SMK Wikrama Bogor</h3>  
+          <div class="d-flex justify-content-center">
+          <div class="card " style="width: 30rem;">
+                <img src="assets/img/gdg.jpg" class="card-img-top " alt="...">
+            </div>
+            </div>
+            <div class="card-body d-flex justify-content-center">
+              <p class="card-text w-75 p-1 text-white">
+ 	
+                SMK Wikrama Bogor didirikan oleh Ir. Itasia Dina Sulvianti dan Dr.H.RP Agus Lelana dibawah naungan Yayasan Prawitama pada tahun 1996 di bekas gudang KUD. Kompetensi keahlian yang pertama dibuka pada saat itu adalah sekretaris dengan jumlah hanya 34 siswa.
+                
+                Seiring berjalannya waktu, jumlah siswa SMK Wikrama Bogor setiap tahunnya terus bertambah. Sehingga pada tahun 2001, secara bertahap SMK Wikrama Bogor menempati gedung yang lebih luas diatas tanah ± 5000m2, berlokasi di Jalan Raya Wangun Kelurahan Sindangsari Kota Bogor. Hingga saat ini, SMK Wikrama Bogor  memiliki 1596 siswa dengan 51 guru pendidik.
+                
+                Kompetensi keahlian di SMK Wikrama Bogor pun terus berkembang. SMK Wikrama Bogor membuka 7 kompetensi keahlian, diantaranya (1) Manajemen Perkantoran dan Layanan Bisnis, (2) Pemasaran, (3) Pengembangan Perangkat Lunak dan Gim, (4) Teknik Jaringan Komputer dan Telekomunikasi, (5) Desain Komunikasi Visual, (6) Kuliner, dan (7) Perhotelan.
+                
+                Kesuksesan SMK Wikrama Bogor saat ini tentunya tidak lepas dari sejarah SMK Wikrama Bogor  mulai dari membentuk visi dan misi, kerja keras hingga diakui dunia internasional hingga prestasi dan pengharagaan yang didapatkan SMK Wikrama Bogor sejak awal didirikan..</p>
+            </div>
+      </div>
+    </div>
+      <hr>
+      <!-- Education List -->
+      <div class="d-flex justify-content-center " style="background: #3572EF">
+
+          <section class="section section-small text-white ">
+            <h2 class="section-header">Education</h2>
+            <h5>  SMPN 2 ciawi</h5>
+            <p>Siswa | Jul 2020 - mei 2023</p>
+            <h5>  Smk Wikrama</h5>
+            <p>Siswa | jun 2023 - jul 2026</p>
+          </section>
+      </div>
+      <hr>
+@endsection
